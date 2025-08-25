c691747ddfd6c3f74152859e87ed15de0af7fcda
Thulir Tech <thulirtechdev@gmail.com>
2025-08-25 17:23:31 +0000

    The formatted INR is not working, better Try hardcoding INR symbol

--- src/lib/format.ts
+++ src/lib/format.ts
@@ -1,10 +1,8 @@
 
 export function formatCurrency(amount: number) {
   const formattedAmount = new Intl.NumberFormat('en-IN', {
-    style: 'currency',
-    currency: 'INR',
     minimumFractionDigits: 0,
     maximumFractionDigits: 0,
   }).format(amount);
-  return formattedAmount.replace('₹', '₹');
+  return `₹${formattedAmount}`;
 }
