4e4465928bfa2826a78524d49ee6978ed4c01468
Thulir Tech <thulirtechdev@gmail.com>
2025-08-27 11:15:33 +0000

    Can you please reduce the gap little bit between the 3 icons in header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -83,7 +83,7 @@ export default function Header() {
             Jouwwinkel
           </Link>
         </div>
-        <div className="flex items-center gap-2">
+        <div className="flex items-center gap-1">
           <SearchDialog />
           <UserNav />
           <Button variant="ghost" size="icon" className="relative" asChild>
