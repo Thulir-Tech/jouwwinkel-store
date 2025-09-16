e52dd9ce7e9c509c44a2e7bb19cc9071878b8ce9
Thulir Tech <thulirtechdev@gmail.com>
2025-09-16 18:56:42 +0000

    const firebaseConfig = {   apiKey: "AIzaSyChoQYG9z1tWhih1G1KvqGNzEPkb9IG

--- src/lib/firebase.client.ts
+++ src/lib/firebase.client.ts
@@ -6,12 +6,12 @@ import { getStorage } from "firebase/storage";
 // import { getAnalytics, isSupported } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyChoQYG9z1tWhih1G1KvqGNzEPkb9IG624",
-  authDomain: "jouwwinkel.firebaseapp.com",
-  projectId: "jouwwinkel",
-  storageBucket: "jouwwinkel.appspot.com",
-  messagingSenderId: "537625442440",
-  appId: "1:537625442440:web:6dae4fc624971b592d073d"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
